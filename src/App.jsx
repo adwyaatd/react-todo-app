005f6358ecdcf98ce48359d163119b4ff3b9995f
細田 来夢 <52363465+adwyaatd@users.noreply.github.com>
2021-09-30 22:45:31 +0900

    7-54

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,36 @@ import React, { useState } from "react";
 import "./styles.css";
 
 export const App = () => {
+  const [todoText, setTodoText] = useState("");
+
   const [incompleteTodos, setIncompleteTodos] = useState([
     "あああああ1",
     "いいいいい2"
   ]);
+
   const [completeTodos, setCompleteTodos] = useState([
     "完了したやつ１",
     "完了したやつ2"
   ]);
 
+  const onChangeTodoText = (event) => setTodoText(event.target.value);
+
+  const onClickAdd = () => {
+    if (todoText === "") return;
+    const newTodos = [...incompleteTodos, todoText];
+    setIncompleteTodos(newTodos);
+    setTodoText("");
+  };
+
   return (
     <>
       <div className="input-area">
-        <input placeholder="TODOを入力" />
-        <button>追加</button>
+        <input
+          placeholder="TODOを入力"
+          value={todoText}
+          onChange={onChangeTodoText}
+        />
+        <button onClick={onClickAdd}>追加</button>
       </div>
 
       <div className="incomplete-area">
